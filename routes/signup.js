d493fdb80897a6ff14a4915cc54e27ba7c4f23bf
Pascall De Creator <pascall.de.creator@gmail.com>
2024-05-11 11:45:42 +0000

    Add signup route

---
+++ routes/signup.js
@@ -0,0 +1,40 @@
+let express = require("express");
+
+let router = express.Router();
+let userModel = require("../models/usermodel");
+
+router.use(express.json());
+router.use(express.urlencoded({ extended: true }));
+
+router.get("/", (req, res)=>{
+    res.render('signup');
+})
+
+router.post("/", async (req, res)=>{
+    try{
+        const Username = await req.body.username;
+        const Password = await req.body.password;
+        const userExists = await userModel.findOne({ username: Username });
+
+        if (userExists) {
+            err = "username has bee taken"
+            console.log(err);
+            return res.redirect('/signup', { errors: [ err ] });
+        }
+
+        let user = new userModel({
+            username: Username,
+            password: Password,
+        });
+
+        let savedUser = await user.save();
+        console.log(savedUser);
+        
+        res.redirect("/");
+    } catch(err) {
+        console.log(err.message);
+        res.render("signup", { errors: [ err.message ] })
+    }
+});
+
+module.exports = router;
